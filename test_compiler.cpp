1d7e9dcfafc98a36851cacddb7d9924c9bbd24ea
brian.kim <brian.kim@beatthemarket.co.kr>
2019-01-23 07:56:52 +0900

    more enhancements #4

---
+++ test_compiler.cpp
@@ -0,0 +1,13 @@
+//
+// Created by BongsuKim on 2019-01-02.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str = "Try to compile";
+    std::cout << str << '\n';
+    return 0;
+}
